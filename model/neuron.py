a29809ca0aa76ad8ef84fa55cf74a92accb44682
MIN CHANGZHUO <45233694+peterpan1998@users.noreply.github.com>
2025-11-27 09:43:22 +0800

    Update

--- model/neuron.py
+++ model/neuron.py
@@ -2,46 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 class DualThresholdSelfregulatingIntegrateFunction(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx,inputs,activation,dt=0.001,initial_state=None,spiking_aware_training=True,return_sequences=False,training=False,T=1):
-        step = T
-        ctx.activation = activation
-        ctx.return_sequences = return_sequences
-        ctx.save_for_backward(inputs)
-        device = inputs.device
-        if training and not spiking_aware_training:
-            output = activation(inputs if return_sequences else inputs[:, -1])
-            return output
-        if initial_state is None:
-            initial_state = torch.rand(
-                inputs.shape[0], inputs.shape[2], dtype=inputs.dtype, device=device
-            )
-        inputs = inputs.to(dtype=initial_state.dtype)
-        voltage = initial_state
-        all_spikes = []
-        rates = activation(inputs) * dt
-        for i in range(inputs.shape[1]):
-            for _ in range(step):
-                voltage += rates[:, i]
-                n_spikes = torch.floor(voltage)
-                voltage -= n_spikes
-            if return_sequences:
-                all_spikes.append(n_spikes)
-        if return_sequences:
-            output = torch.stack(all_spikes, dim=1)
-        else:
-            output = n_spikes
-        output /= dt
-        return output
-    @staticmethod
-    def backward(ctx, grad_output):
-        inputs = ctx.saved_tensors[0]
-        with torch.enable_grad():
-            output = ctx.activation(inputs if ctx.return_sequences else inputs[:, -1])
-            return (
-                torch.autograd.grad(output, inputs, grad_outputs=grad_output)
-                + (None,) * 7
-            )
+    #There is a problem with the previous one, which will be uploaded after modification
+    pass
 class DualThresholdSelfregulatingIntegrate(torch.nn.Module):  # pylint: disable=abstract-method
     def __init__(self,activation,dt=0.001,initial_state=None,spiking_aware_training=True,return_sequences=True,T=1):
         super().__init__()
